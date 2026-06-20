53aaa85e3c8cdf2a23262ed4af1bec66ceeafc2b
sly <maltomare70@gmail.com>
2026-06-20 23:15:26 +0200

    Login

---
+++ EmmaClientAv/Forms/Login/Login.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace EmmaClientAv.Forms.Login;
+
+public partial class Login : Window
+{
+    public Login()
+    {
+        InitializeComponent();
+    }
+}
